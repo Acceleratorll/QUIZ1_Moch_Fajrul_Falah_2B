6a93f9e6b6af8245b3df0a9372cdbd0864f07845
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2022-03-21 14:10:00 +0700

    Membuat banyak partials view

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -37,23 +37,17 @@
 
     </div>
   </header><!-- End Header -->
-  
-  <div class="hero hero-single route bg-image" style="background-image: url(assets/img/overlay-bg.jpg)">
-    <div class="overlay-mf"></div>
+  <!-- ======= Hero Section ======= -->
+  <div id="hero" class="hero route bg-image" style="background-image: url(assets/img/hero-bg.jpg)">
+    <div class="overlay-itro"></div>
     <div class="hero-content display-table">
       <div class="table-cell">
         <div class="container">
-          <h2 class="hero-title mb-4">Blog Details</h2>
-          <ol class="breadcrumb d-flex justify-content-center">
-            <li class="breadcrumb-item">
-              <a href="#">Home</a>
-            </li>
-            <li class="breadcrumb-item">
-              <a href="#">Library</a>
-            </li>
-            <li class="breadcrumb-item active">Data</li>
-          </ol>
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h1 class="hero-title mb-4">I am Moch Fajrul Falah</h1>
+          <p class="hero-subtitle"><span class="typed" data-typed-items="Programmer, Developer, Freelancer, Gamer"></span></p>
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
         </div>
       </div>
     </div>
-  </div>
+  </div><!-- End Hero Section -->
